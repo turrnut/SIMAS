520001392c233f705371e809631011c9edc2c98b
turrnut <victorconqueror021@gmail.com>
2024-12-14 13:53:48 -0500

    Update common.js

--- common.js
+++ common.js
@@ -1,6 +1,6 @@
 // a file that is included in both compiler.js and program.js
 
-const version = "0.0.2"
+const version = "0.9"
 const compiler_information = "SIMAS Programming Language Compiler v" + version + "\nCopyright (c) 2024 Turrnut\n";
 const runtime_information = "SIMAS Programming Language v" + version + "\nCopyright (c) 2024 Turrnut\n";
 const xkey = 71;
